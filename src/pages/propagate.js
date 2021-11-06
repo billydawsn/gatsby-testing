a2a1a799e04ddbd904bc1a204d5635d955e3969a
Billy <billydawson98@gmail.com>
2021-11-06 16:03:45 +0000

    Test Blog Post

---
+++ src/pages/propagate.js
@@ -0,0 +1,219 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <Seo title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <StaticImage
+      src="../images/Plant.webp"
+      width={573}
+      height={430}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt="A Gatsby astronaut"
+      style={{ marginBottom: `1.45rem` }}
+    />
+
+    <span className="Content">
+    Polka Dot Plants (Hypoestes phyllostachya) are some of the most colourful and unique houseplants out there. You can find them in a variety of colours, from pink, white and even red and are named simply because of their incredible dotty leaves! Polka Dot Plants can be easy to care for but are even easier to propagate. This post will guide you through everything you need to propagate your Polka Dot Plant, including a step by step process, tools required, how to spot and solve problems that creep up and answers to all of your propagation FAQs.
+
+Why propagate a Polka Dot Plant?
+Before we get into the nitty-gritty of Polka Dot Plant propagation, you might be wondering why it’s beneficial to propagate a Polka Dot Plant in the first place? Well here are some of the main reasons:
+
+
+
+Create more Polka Dot Plants for free!
+
+Propagating your Polka Dot Plant will multiply your plant collection without needing to spend any more money! They also make great gifts for friends and family if you already have enough houseplants.
+
+Save a dying Polka Dot Plant.
+
+If you accidentally overwatered (which is easy to do), over-fertilised or mistreated your Polka Dot Plants and it has started to die, sometimes the best way to save it is by taking the healthy stems and propagating them to create new healthy plants before it’s too late.
+
+To make your Polka Dot Plants fit their environment.
+
+Polka Dot Plants can grow pretty quickly if in the right environment and they can slowly outgrow their pot or spot in your home. Giving them a trim is a great way to cut back some of the growth and propagating those Polka Dot plant stems is a great way to stop them from going to waste.
+
+What tools will I need to propagate my Polka Dot Plant?
+Let’s start off with the easy part, what you need to propagate a Polka Dot Plant. It’s important to make sure you have everything ready before taking any cuttings.
+
+
+
+Healthy and mature Polka Dot Plant
+
+Clean, sharp scissors/shears
+
+Spare pot(s) with and without drainage holes
+
+Fresh soil and water
+
+Newspaper or plastic sheet if you are propagating indoors
+
+Rooting hormone (optional)
+
+Gardening gloves (optional)
+
+Propagation Starter Kit
+Here are some great, affordable products that should help you successfully propagate your Polka Dot Plant. They are also super helpful for general plant care so are a good investment to make sure your urban jungle flourishes.
+
+How to Propagate a Polka Dot Plant main image
+Propagation Station
+Keep an eye on how quickly your cuttings are growing roots!
+
+POPULAR
+How to Propagate a Polka Dot Plant main image
+Rooting Gel
+Encourages stronger and faster root growth
+
+How to Propagate a Polka Dot Plant main image
+Heat Pad
+Support healthy growth by keeping your cuttings warm
+
+How to propagate a Polka Dot Plant using stem cuttings
+This is the main method of propagating Polka Dot Plants as it’s suitable for all maturities. It’s also really easy to get right as you can create several new Polka Dot Plants just by taking a few small cuttings
+
+
+
+1
+
+Locate some healthy Polka Dot stems
+
+When taking a vine cutting you want to make sure that the part of the plant you are cutting is healthy to give you any chance of success. Avoid any sign of disease or pests as they will be transferred onto your new cuttings and may make it harder for the cuttings to grow into proper plants.
+
+When taking cuttings to propagate your Polka Dot Plant, you need to check for one important thing; a node. Without it, you will have a very slim chance of a successful propagation. A node is where a leaf has grown from the stem.
+
+2
+
+Make the cut
+
+Use your clean scissors to cut off one or more stems. You want to make sure that you have at least 2-3 leaves on your cutting as this ensures you have a few good nodes from which roots can grow out from.
+
+Keeping your tools clean avoids passing on bacteria or pests to your plant and also is important when dealing with toxic plants. (Although Polka Dot Plants are largely non-toxic, it’s a good habit to get yourself into)
+
+3
+
+Fill up a transparent container with water
+
+Now that you have taken your cuttings, you need to grow them in something. You can pot them directly into soil as Polka Dot Plants grow roots very quickly but we always recommend water as a middle step. It just has a better success rate.
+
+Fill up your container with temperate water (so as not to shock your delicate cuttings) and place your cuttings in so that the lowest third is covered by the water. Covering any more of the cutting increases the chance it will rot. If any leaves will be in contact with the water then remove them at this step as they will start to rot after being submerged for a few days.
+
+We always prefer to use a clear container when propagating as you can see everything a little clearer.
+
+4
+
+Place your cuttings in bright but indirect light
+
+Place your glass in bright but indirect sunlight. Intense light will damage the cutting and prevent a successful propagation. You can use a light monitor to figure out if you’re Philodendron Birkin cuttings are getting the right amount of sunlight. You can also supplement sunlight levels by using an LED grow light which are great at encouraging growth in cuttings. We love this one which is available on Amazon here.
+
+It’s at this stage that you might choose to use rooting hormone. This will help to speed up root growth on the new cuttings. We have always found this rooting gel to be successful – you can buy it on Amazon here.
+
+5
+
+Change out the water regularly
+
+One of the most important steps in the Polka Dot Plant propagation process is to refresh the water regularly (every 2-3 days is optimal). This keeps the water free from bacteria and stops it from stagnating which is harmful to your cutting. Stagnant water will also start to smell so it’s best to avoid this!
+
+6
+
+Be patient
+
+Luckily Polka Dot Plant cuttings grow roots pretty quickly so you should start to see two delicate roots pop out from the node after only a few days. It can be a little unpredictable though and some cuttings will be a lot faster than others. But the process is definitely a lot quicker than with some other common houseplants, where you can be waiting months for anything to happen.
+
+The benefit of Polka Dot Plants growing roots so quickly is that there is less time for something to go wrong and the risk of root rot is significantly reduced.
+
+7
+
+Plant your Polka Dot cuttings into fresh potting mix
+
+Once the roots on your Polka Dot cuttings are a few centimetres long it’s time to pot them into soil. Don’t worry if the roots are still very delicate, this is normal for Polka Dot plants so just be a little careful when potting them to make sure they don’t break.
+
+We recommend using a high-quality potting mix to make sure your cuttings are getting the right mix of nutrients. Our top choice for soil would be this potting mix from Miracle Gro. Carefully place your cuttings a few centimetres into the soil. You can also pop the cuttings back into the mother plant so it becomes a little fuller – it’s up to you!
+
+8
+
+Resume usual Polka Dot Plant care
+
+Now that your cuttings are pot nicely into soil, you can go back to your usual Polka Dot Plant care routine and soon those plants will be big enough to propagate again and you’ll end up with way more Polka Dot Plants than you could ever need…
+
+Propagate a Polka Dot Plant through division of the mother plant
+This method of propagation only really works if your Polka Dot Plant is quite bushy, otherwise, they can be left looking a little straggly. If you do have a big enough Polka Dot Plant that you don’t mind dividing then this method is great for those who don’t want to wait for new roots and leaves to grow – instead, you get new plants instantly!
+
+
+
+1
+
+Take your Polka Dot Plant out of its pot
+
+To be able to propagate your Polka Dot Plant through the division method, you need to separate the different offshoots/stems. This is particularly easy to do with Polka Dot Plants compared to some other plants as they separate easily.
+
+The first step is to carefully lift your plant out of its pot. Don’t tug too hard at the leaves as they are quite delicate but slowly shimmy the plant out of the pot.
+
+2
+
+Separate out the plant
+
+You may have to trim off the odd root to detangle the plant but you should be able to pull the various parts of the plant apart. Each section should have some roots attached so inspect these carefully to make sure there aren’t any obvious problems (mushy rotted roots is the main one you are looking out for here).
+
+How many new plants you want to create is completely up to you and totally depends on your mother plant. When separating parts of the plant, make sure to always use shears rather than pulling the plant apart as this can damage it quite easily!
+
+3
+
+Place each new plant in fresh potting mix
+
+Pop your mother Polka Dot Plant back into its original pot (or downsize slightly if you have taken away a substantial amount of the original plant). Then pot your new plant(s) into fresh potting mix. This is our favourite potting mix from Miracle Gro.
+
+Make sure not to reuse old potting mix from other plants as there may be bad bacteria or pests lingering in it that will start to impact the health of your new plants.
+
+4
+
+Continue normal Polka Dot Plant care
+
+That’s all there is to the division propagation method of Polka Dot Plants. Now that your plants are safely in their new homes, continue with your normal Polka Dot Plant care routine!
+
+Polka Dot Plant Propagation FAQs
+
+Here’s a few of the most common questions we find people have when looking to propagate their Polka Dot Plants.
+
+How long will it take for my Polka Dot Plant cuttings to grow roots?
+Read more
+What’s the best time of year to propagate Polka Dot Plants?
+Read more
+What soil should I use when propagating a Polka Dot Plant?
+Read more
+Can I use a rooting hormone when propagating a Polka Dot Plant?
+Read more
+Should I use a grow light for my Polka Dot Plant cuttings?
+Read more
+Do I need to fertilise my Polka Dot Plant cuttings?
+Read more
+Can I propagate a Polka Dot Plant from a single leaf?
+Read more
+What is a node?
+Read more
+Common problems when propagating Polka Dot Plants
+
+Propagating plants doesn’t always have 100% success and you may encounter some problems along the way. But don’t worry, below you can figure out what is causing these problems to arise and hopefully solve them before it kills your plant cuttings.
+
+Why isn’t my Polka Dot Plant cutting growing roots?
+Read more
+Why is my cutting turning mushy?
+Read more
+Why are the new leaves on my cutting pale?
+Read more
+We hope you have found this guide to Polka Dot Plant propagation useful. It can sometimes be a little unpredictable whether or not it will be a success but this is definitely easier than propagating some other houseplant types so we are sure you will have Polka Dot Plants growing left, right and centre in no time at all!!
+
+Check out our full Polka Dot Plant Care Guide that has all the information on how to continue care for your cuttings once it has matured.
+    </span>
+
+  </Layout>
+)
+
+export default IndexPage
